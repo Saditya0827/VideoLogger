9534958e99c279382e7387855087ab058e182ba4
Saditya0827 <Saditya0827@github.com>
2023-10-22 12:41:58 +0530

    intial commit for videoLogger feature contains record video

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddVideoLogComponent } from './component/add-video-log/add-video-log.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddVideoLogComponent
   ],
   imports: [
     BrowserModule,
